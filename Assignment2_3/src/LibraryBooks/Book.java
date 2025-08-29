9f9c002627e44951b72de01d6cd853ef15fd9b18
kumudun <kumudun@metropolia.fi>
2025-08-29 04:07:37 +0300

    Assignment2_3-Task 4

--- Assignment2_3/src/LibraryBooks/Book.java
+++ Assignment2_3/src/LibraryBooks/Book.java
@@ -1,16 +1,40 @@
 package LibraryBooks;
+import java.util.ArrayList;
+
+
 
 public class Book {
+
     private String title;
     private String author;
     private int publicationYear;
+    private double rating;
+    private ArrayList<String> reviews = new ArrayList<>();
 
     // Constructor
     public Book(String title, String author, int publicationYear) {
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
+        this.rating = rating;
+        this.reviews = reviews;
     }
+
+
+
+    // set rating
+    public void setRating(double rating) {
+        if (rating >= 0 && rating <= 5) {
+            this.rating = rating;
+        }
+    }
+
+    // Add review
+    public void addReview(String review) {
+        reviews.add(review);
+    }
+
+
 
     // Getter for title
     public String getTitle() {
@@ -27,8 +51,24 @@ public class Book {
         return publicationYear;
     }
 
+    // Getter for rating
+    public double getRating() {
+        return rating;
+    }
+
+    // Getter review
+    public ArrayList<String> getReviews() {
+        return reviews;
+    }
+
+
 
     public void printBook() {
-        System.out.println("Title: " + title + ", Author: " + author + ", Year: " + publicationYear);
+        System.out.println("Title: " + title + ", Author: " + author + ", Year: " + publicationYear + ", Rating: " + rating + ", Reviews: " + reviews);
     }
 }
+
+
+
+
+
